52ab2ec9be44bb0ef275d05c70cc579ecae0b80c
Cristofer Quiroz <ceql@live.cl>
2025-07-22 11:39:27 -0400

    Se sube prueba de router

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
+  ]
 };
