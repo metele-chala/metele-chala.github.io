b6a6af2e9e0e57aa879f19d26fdc48e8fd51263d
Cristofer Quiroz <ceql@live.cl>
2025-07-22 11:02:40 -0400

    Se sube prueba de build

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'claseb' title`, () => {
+  it(`should have the 'metele-chala' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('claseb');
+    expect(app.title).toEqual('metele-chala');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, claseb');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, metele-chala');
   });
 });
